08955d38a26491714406eebff6dffbfca47ebe40
dryaed <ed.v.morozov@gmail.com>
2023-03-30 03:44:03 +0200

    a

--- Assets/_Scripts/TouchManager.cs
+++ Assets/_Scripts/TouchManager.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
-using UnityEditor.Rendering.LookDev;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
